f154295e95bc2055a9794956992860565cddaa1a
nidhimishra-hashicorp <86828798+nidhimishra-hashicorp@users.noreply.github.com>
2022-01-07 01:45:58 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  filter {
+filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  filter {
+filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
